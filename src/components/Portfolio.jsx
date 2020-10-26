c4ddd9a8732ed8bab6758cc5787861f4ae591242
alex rodriguez <alexcarvajal2404@gmail.com>
2020-10-26 16:37:15 -0500

    0.03

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -51,55 +51,55 @@ const tileData = [
   {
     image: 'https://firebasestorage.googleapis.com/v0/b/lsm-1-46b3d.appspot.com/o/lsm%2Fpribcess.png?alt=media&token=b32de831-66af-4be8-ad64-fd17c7739eee',
     title: 'PrettyPrincess.com',
-    cols: 1,
+    url:'https://prettyprincess.pe',
     id: 1
   },
   {
     image: 'https://firebasestorage.googleapis.com/v0/b/lsm-1-46b3d.appspot.com/o/lsm%2FLugbrand.png?alt=media&token=092dab48-2c8b-44b2-a9cc-152acf967c8e',
     title: 'Lugbrand.com',
-    cols: 1,
+    url:'https://google.com',
     id: 2
   },
   {
     image: 'https://firebasestorage.googleapis.com/v0/b/lsm-1-46b3d.appspot.com/o/lsm%2Fgrupo%20de%20ingenieros.png?alt=media&token=6455a5e4-8c31-4e46-9a5d-4733c94d69be',
     title: 'GrupodeIngenieros.com',
-    cols: 1,
+    url:'https://grupodeingenieros.com',
     id: 3
   },
   {
     image: 'https://firebasestorage.googleapis.com/v0/b/lsm-1-46b3d.appspot.com/o/lsm%2Fgrupo%20kiel.png?alt=media&token=c9227274-66a7-4bb0-862e-78e1e45f242a',
     title: 'GrupoKiel.com',
-    cols: 1,
+    url:'https://grupokiel.com.pe',
     id: 4
   },
   {
     image: 'https://firebasestorage.googleapis.com/v0/b/lsm-1-46b3d.appspot.com/o/lsm%2Fnutripharma.png?alt=media&token=66e8a78d-dcb2-4863-8757-05f73deb5d21',
     title: 'NutriPharmasac.com',
-    cols: 1,
+    url:'http://nutripharmasac.com/',
     id: 5
   },
   {
     image: 'https://firebasestorage.googleapis.com/v0/b/lsm-1-46b3d.appspot.com/o/lsm%2Fcredix.png?alt=media&token=436895a2-ea0d-4aac-99fa-16d5a739ebb7',
     title: 'Credix.com',
-    cols: 1,
+    url:'https://www.credix.com/',
     id :6
   },
   {
     image: 'https://firebasestorage.googleapis.com/v0/b/lsm-1-46b3d.appspot.com/o/lsm%2Fvanesaduran.png?alt=media&token=07c7ecd0-17cb-4346-90c1-2482730345de',
     title: 'VanessaDuran.com',
-    cols: 1,
+    url:'https://vanessaduran.com',
     id: 7
   },
   {
     image: 'https://firebasestorage.googleapis.com/v0/b/lsm-1-46b3d.appspot.com/o/lsm%2Fprwireless.png?alt=media&token=cd379257-01b5-44ae-ba25-1288f9f937e1',
     title: 'Prwireless.com',
-    cols: 1,
+    url:'www.prwireless.net',
     id: 8
   },
   {
     image: 'https://firebasestorage.googleapis.com/v0/b/lsm-1-46b3d.appspot.com/o/lsm%2Fopenmobile.png?alt=media&token=6a6e5e3d-22c9-41a3-8fd9-b4ce343e73fc',
     title: 'OpenMobile.com',
-    cols: 1,
+    url:'https://openmobile.com',
     id: 9
   },
   
@@ -121,7 +121,7 @@ function Portfolio() {
               <Typography variant="h6" color="secondaryText" align="center">
                 {tile.title}
               </Typography>
-              <IconButton>
+              <IconButton onClick={() => window.open(tile.url)}>
                 <img src={tile.image} alt={tile.title} className={classes.image} />
               </IconButton>
             </GridListTile>
