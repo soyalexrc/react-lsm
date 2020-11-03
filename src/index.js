8e030bd1cfd602850502958b49e67ee03cff02ea
alex rodriguez <alexcarvajal2404@gmail.com>
2020-11-02 20:53:26 -0500

    version deploymen

--- src/index.js
+++ src/index.js
@@ -2,20 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-// import Prueba from './components/Prueba'
-// import * as serviceWorker from './serviceWorker';
-// import "slick-carousel/slick/slick.css"; 
-// import "slick-carousel/slick/slick-theme.css";
+
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    {/* <Prueba /> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
