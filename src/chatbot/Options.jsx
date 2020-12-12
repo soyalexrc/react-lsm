4f38efc54f7deea077ab3d7153c62932bb3bb910
alex rodriguez <alexcarvajal2404@gmail.com>
2020-12-12 18:36:58 -0500

    [pending] luis revisar cambios

--- src/chatbot/Options.jsx
+++ src/chatbot/Options.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Button, ButtonGroup } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
-import { Link as SmoothLink } from 'react-scroll'
 
 const useStyles = makeStyles({
   buttonGroup : {
